2f39fe499a66d2f8901e4e216960dc35a7e63da0
Marcos Cavalcante <marcos.bcc2011@gmail.com>
2017-12-04 07:21:27 +0000

    Setting up app and getting data

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD2Jvl6-AYF08qiJyey-HVONOmeHyFwmPw",
+    authDomain: "fs1pr-f7190.firebaseapp.com",
+    databaseURL: "https://fs1pr-f7190.firebaseio.com",
+    projectId: "fs1pr-f7190",
+    storageBucket: "fs1pr-f7190.appspot.com",
+    messagingSenderId: "289901655334"
+  }
 };
